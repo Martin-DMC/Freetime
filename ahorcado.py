dee25b2950110d5d2e1b6b1ee19d4104f0792d7b
Martin-DMC <the.chiperfil@gmail.com>
2025-06-12 14:43:37 +0000

    mejoras en ahorcado

--- ahorcado.py
+++ ahorcado.py
@@ -3,7 +3,7 @@ import random
 import os
 
 def secret_word():
-    palabras = [('amarillo'), ('rojo'), ('verde'), ('negro')]
+    palabras = [('terminator'), ('avatar'), ('chuky'), ('casocerrado'), ('elconjuro'), ('superman')]
     ran = random.randint(0, len(palabras) - 1)
     palabra = palabras[ran]
     return palabra
@@ -25,6 +25,8 @@ def llenar_matrix(matrix, vidas):
         matrix[2][0] = '/'
     elif vidas == 1:
         matrix[1][1] = 'O'
+    elif vidas == 0:
+        matrix[0][1] = '|'
     return matrix
 
 def bienvenida():
@@ -47,8 +49,22 @@ def print_tablero(matrix):
         ret = "".join(fila)
         print(ret)
 
+def check_word(palabra, palabra1):
+    if palabra == palabra1:
+        return True
+    else:
+        return False
+
+def salir():
+    op = input("[s] para salir, [enter] para continuar")
+    if op == 's':
+        return True
+    else:
+        return False
 
 vidas = 7
+i = 0
+letras_usadas = []
 palabra = secret_word()
 pal_jug = word_player(palabra)
 matrx = [[' ', ' ', ' '],
@@ -57,16 +73,32 @@ matrx = [[' ', ' ', ' '],
 [' ', ' ', ' ']]
 limpiar()
 bienvenida()
-while True or vidas > 0:
+while i < 100 and vidas > 0:
     limpiar()
-    print_tablero(matrx)
-    print("\ntu palabra")
     cadena_jug = "".join(pal_jug)
+    if check_word(palabra, cadena_jug) is True:
+        print("""\n\n        *************
+        * you win!! *
+        *************""")
+        break
+    else:
+        pass
+    print_tablero(matrx)
+    if i != 0:
+        print("letras usadas")
+        print(letras_usadas) 
+    print("\ntu palabra")
     print(cadena_jug)
     letra = input("\ningresa una letra: ")
-    asierto = 1
+    while True:
+        if letra in letras_usadas:
+            letra = input("\nletra ya ingresada: ")
+        else:
+            break
+        limpiar()
     if not letra in palabra:
-        vidas -= 1        
+        vidas -= 1
+        letras_usadas.append(letra)
     for i, let in enumerate(palabra):
         if let == letra:
             pal_jug[i] = let
@@ -76,4 +108,12 @@ while True or vidas > 0:
             else:
                 pal_jug[i] = '-'
     llenar_matrix(matrx, vidas)
-    input("pres enter")
+    if salir() is True:
+        break
+    else:
+        continue
+if vidas == 0:
+    llenar_matrix(matrx, vidas)
+    limpiar()
+    print_tablero(matrx)
+    print("you lose. BUUHHH!!!")
